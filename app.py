aca0ce36af72d7f8868c40e71d01dd78321a3d0c
dd <dfernandez.mca15@lania.edu.mx>
2016-07-17 17:11:33 -0500

    committt

--- app.py
+++ app.py
@@ -10,15 +10,6 @@ def main():
 def index():
   return render_template('index.html')
 
-@app.route('/hi')
-def hello_world_lulu():
-    # this is a comment, just like in Python
-    # note that the function name and the route argument
-    # do not need to be the same.
-    r = [1,2,3,4,5,6,7,8]
-    
-    return str(np.random.choice(r))
-
 
 if __name__ == '__main__':
   app.run(port=33507)
